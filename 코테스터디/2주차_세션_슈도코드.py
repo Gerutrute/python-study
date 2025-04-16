4defa54a4934ebdce2a47e049e9f14278af35a25
Gerutrute <96192262+Gerutrute@users.noreply.github.com>
2025-04-16 12:19:07 +0900

    Create 2주차_세션_슈도코드.py

---
+++ 코테스터디/2주차_세션_슈도코드.py
@@ -0,0 +1,17 @@
+"""
+1. itertools을 import
+2. speed, efficiency 리스트 작성
+3. comb_speed_list 빈 리스트 작성
+4. comb_effi_list 빈 리스트 작성
+5. nCr 함수 작성, itertools.combination 사용
+6. speed에 슬라이싱 [0]으로 한 range를 만들고 for문 작성
+7. combi_speed 변수 만들고 nCr(n의 수, r의 값 입력)
+8. com_speed_list 변수 만들고 combi_speed를 comb_speed_list에 append
+9. combi_effi 변수 만들고 nCr(n의 수, r의 값 입력)
+10. com_effi_list 변수 만들고 combi_effi를 comb_effi_list에 append
+11. zip함수에 comb_speed_list, comb_effi_list 입력하고 for문으로 변수 2개 입력
+12. zip함수에 11.에서 입력한 변수 2개 입력하고 for문으로 변수 2개 입력
+13. 만약 12. for문 변수 중 하나가 True라면(변수 값 null 방지)
+14. 12 함수의 변수 2개 출력 speed 요소 변수 sum 출력, efficiency 요소 변수 min 출력
+15. sum하고 min 함수 곱한 값 출력
+"""
